c056af6aac82f8681a609ee0c8a87816ec26fb45
BinhPV9 <binhpv9@fpt.com>
2025-01-10 11:33:36 +0700

    update

--- src/main/java/com/company/learn_jmix/security/EmployeeRole.java
+++ src/main/java/com/company/learn_jmix/security/EmployeeRole.java
@@ -1,8 +1,33 @@
 package com.company.learn_jmix.security;
 
+import com.company.learn_jmix.entity.Step;
+import com.company.learn_jmix.entity.User;
+import com.company.learn_jmix.entity.UserStep;
+import io.jmix.security.model.EntityAttributePolicyAction;
+import io.jmix.security.model.EntityPolicyAction;
+import io.jmix.security.role.annotation.EntityAttributePolicy;
+import io.jmix.security.role.annotation.EntityPolicy;
 import io.jmix.security.role.annotation.ResourceRole;
+import io.jmix.securityflowui.role.annotation.MenuPolicy;
+import io.jmix.securityflowui.role.annotation.ViewPolicy;
 
-@ResourceRole(name = "Employee", code = EmployeeRole.CODE, scope="UI")
+@ResourceRole(name = "Employee", code = EmployeeRole.CODE, scope = "UI")
 public interface EmployeeRole {
     String CODE = "employee";
+
+    @EntityPolicy(entityClass = Step.class, actions = EntityPolicyAction.READ)
+    @EntityAttributePolicy(entityClass = Step.class, attributes = "*", action = EntityAttributePolicyAction.VIEW)
+    void step();
+
+    @EntityAttributePolicy(entityClass = User.class, attributes = "*", action = EntityAttributePolicyAction.VIEW)
+    @EntityPolicy(entityClass = User.class, actions = {EntityPolicyAction.READ, EntityPolicyAction.UPDATE})
+    void user();
+
+    @EntityAttributePolicy(entityClass = UserStep.class, attributes = "*", action = EntityAttributePolicyAction.VIEW)
+    @EntityPolicy(entityClass = UserStep.class, actions = {EntityPolicyAction.READ, EntityPolicyAction.UPDATE})
+    void userStep();
+
+    @MenuPolicy(menuIds = "CustomView")
+    @ViewPolicy(viewIds = "CustomView")
+    void screens();
 }
--- src/main/java/com/company/learn_jmix/security/HRManagerSDepartmentsAndUsersRole.java
+++ src/main/java/com/company/learn_jmix/security/HRManagerSDepartmentsAndUsersRole.java
@@ -1,8 +1,13 @@
 package com.company.learn_jmix.security;
 
+import com.company.learn_jmix.entity.Department;
+import io.jmix.security.role.annotation.JpqlRowLevelPolicy;
 import io.jmix.security.role.annotation.RowLevelRole;
 
 @RowLevelRole(name = "HR manager’s departments and users", code = HRManagerSDepartmentsAndUsersRole.CODE)
 public interface HRManagerSDepartmentsAndUsersRole {
-    String CODE = "hr-manager’s-departments-and-users";
+    String CODE = "hr-manager-rl";
+
+    @JpqlRowLevelPolicy(entityClass = Department.class, where = "{E}.hrManager.id = :current_user_id")
+    void department();
 }
